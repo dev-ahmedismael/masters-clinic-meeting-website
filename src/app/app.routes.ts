b5d772a0008da14ef65da57d9b295d31a9940aa3
Ahmed Ismael <dev.ahmedismael@outlook.com>
2024-08-31 00:58:45 +0300

    Updated page titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,17 @@ import { BookNowComponent } from '../components/book-now/book-now.component';
 import { ReservationInfoComponent } from '../components/reservation-info/reservation-info.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'contact-us', component: ContactUsComponent },
-  { path: 'book-now', component: BookNowComponent },
-  { path: 'reservation-info', component: ReservationInfoComponent },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+    title: 'عيادات ماسترز كلينيك',
+  },
+  { path: 'contact-us', component: ContactUsComponent, title: 'اتصل بنا' },
+  { path: 'book-now', component: BookNowComponent, title: 'احجز الآن' },
+  {
+    path: 'reservation-info',
+    component: ReservationInfoComponent,
+    title: 'عيادات ماسترز كلينيك',
+  },
 ];
